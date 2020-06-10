71a5b63a3aeaada628ba9f59a5bdcefff2a6a6d1
XiaoJiu_company <417987689@qq.com>
2020-06-10 18:33:42 +0800

    + invalid 刷新学习

---
+++ study_pic/src/main/java/com/sn/customView/MyView.java
@@ -0,0 +1,37 @@
+package com.sn.customView;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.Log;
+import android.view.View;
+
+public class MyView extends View {
+
+    public static final String TAG = "MyView";
+
+    public MyView(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        Log.e(TAG, "onLayout");
+        super.onLayout(changed, left, top, right, bottom);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        Log.e(TAG, "onMeasure");
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        Log.e(TAG, "onDraw");
+        super.onDraw(canvas);
+    }
+
+    public void refersh() {
+        invalidate();
+    }
+}
--- study_pic/src/main/java/com/sn/study_pic/StudyAcitivity.java
+++ study_pic/src/main/java/com/sn/study_pic/StudyAcitivity.java
@@ -3,23 +3,27 @@ package com.sn.study_pic;
 import android.content.res.Resources;
 import android.graphics.drawable.TransitionDrawable;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.res.ResourcesCompat;
+
+import com.sn.customView.MyView;
 
 public class StudyAcitivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        CustomDrawableView customDrawableView = new CustomDrawableView(this);
-        setContentView(customDrawableView);
+//        CustomDrawableView customDrawableView = new CustomDrawableView(this);
+//        setContentView(customDrawableView);
 
-        //setContentView(R.layout.activity_study);
+        setContentView(R.layout.activity_study);
 
-        //transitionDrawable();
+        transitionDrawable();
 
     }
 
@@ -28,11 +32,20 @@ public class StudyAcitivity extends AppCompatActivity {
      * 渐变两张图片
      */
     private void transitionDrawable() {
+        final LinearLayout linearLayout = (LinearLayout) findViewById(R.id.ll_study);
         Resources res = getResources();
         TransitionDrawable transition =
                 (TransitionDrawable) ResourcesCompat.getDrawable(res, R.drawable.expand_collapse, null);
 
         ImageView image = (ImageView) findViewById(R.id.toggle_image);
+        image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MyView myView = new MyView(getBaseContext());
+                linearLayout.addView(myView);
+                myView.refersh();
+            }
+        });
         image.setImageDrawable(transition);
         // Description of the initial state that the drawable represents.
         //image.setContentDescription(getResources().getString(R.string.collapsed));
