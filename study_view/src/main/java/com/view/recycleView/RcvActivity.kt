0e7bbda0ea3154eb38c1a1a65b0f7886317a2642
Guoxu <guoxu@em3.ai>
2020-11-04 15:23:30 +0800

    + View-可拖拽的RecyclerView item，

--- study_view/src/main/java/com/view/recycleView/RcvActivity.kt
+++ study_view/src/main/java/com/view/recycleView/RcvActivity.kt
@@ -1,12 +1,18 @@
 package com.view.recycleView
 
-import android.os.Bundle
+import android.app.Service
+import android.os.Vibrator
 import android.view.View
+import android.widget.Toast
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.sn.study_pic.R
 import com.view.BaseDataActivity
 import com.view.StickyScrollView.ListAdapter
+import com.view.recycleView.dragView.OnRecyclerItemClickListener
+import com.view.recycleView.dragView.VerticalDragCallBack
+
 
 class RcvActivity : BaseDataActivity() {
     private var mRecyclerView: RecyclerView? = null
@@ -17,6 +23,24 @@ class RcvActivity : BaseDataActivity() {
         mRecyclerView?.layoutManager = LinearLayoutManager(this)
         mRecyclerView?.adapter = listAdapter
         listAdapter.setData(arrayListData)
+        val verticalDragCallBack = VerticalDragCallBack(arrayListData)
+        val itemTouchHelper = ItemTouchHelper(verticalDragCallBack)
+        itemTouchHelper.attachToRecyclerView(mRecyclerView)
+        mRecyclerView!!.addOnItemTouchListener(object : OnRecyclerItemClickListener(mRecyclerView) {
+            override fun onItemClick(vh: RecyclerView.ViewHolder) {
+                Toast.makeText(baseContext, arrayListData.get(vh.layoutPosition).toString(), Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onItemLongClick(vh: RecyclerView.ViewHolder) {
+                //判断被拖拽的是否是前两个，如果不是则执行拖拽
+                if (vh.layoutPosition != 0 && vh.layoutPosition != 1) {
+                    itemTouchHelper.startDrag(vh)
+                    //获取系统震动服务
+//                    val vib: Vibrator = getSystemService(Service.VIBRATOR_SERVICE) as Vibrator //震动70毫秒
+//                    vib.vibrate(70)
+                }
+            }
+        })
     }
 
     override fun setLayout(): Int {
---
+++ study_view/src/main/java/com/view/recycleView/dragView/VerticalDragCallBack.kt
@@ -0,0 +1,73 @@
+package com.view.recycleView.dragView
+
+import android.graphics.Color
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.RecyclerView
+import java.util.*
+import kotlin.collections.ArrayList
+
+/**
+ * Created by GuoXu on 2020/11/4 14:49.
+ *  参考：https://www.cnblogs.com/wjtaigwh/p/6543354.html
+ */
+class VerticalDragCallBack(var datas: ArrayList<String>) : ItemTouchHelper.Callback() {
+
+    override fun getMovementFlags(recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder): Int {
+        return if (recyclerView.layoutManager is GridLayoutManager) {
+            val dragFlags = ItemTouchHelper.UP or ItemTouchHelper.DOWN or
+                    ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT
+            val swipeFlags = 0
+            makeMovementFlags(dragFlags, swipeFlags)
+        } else {
+            val dragFlags = ItemTouchHelper.UP or ItemTouchHelper.DOWN
+            val swipeFlags = 0
+            makeMovementFlags(dragFlags, swipeFlags)
+        }
+    }
+
+    override fun onMove(recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder, target: RecyclerView.ViewHolder): Boolean {
+        //得到当拖拽的viewHolder的Position
+        val fromPosition = viewHolder.adapterPosition
+        //拿到当前拖拽到的item的viewHolder
+        //拿到当前拖拽到的item的viewHolder
+        val toPosition = target.adapterPosition
+        if (fromPosition < toPosition) {
+            for (i in fromPosition until toPosition) {
+                Collections.swap(datas, i, i + 1)
+            }
+        } else {
+            for (i in fromPosition downTo toPosition + 1) {
+                Collections.swap(datas, i, i - 1)
+            }
+        }
+        recyclerView.adapter?.notifyItemMoved(fromPosition, toPosition)
+        return true
+    }
+
+    override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+    }
+
+    /**
+     * 长按选中Item的时候开始调用
+     *
+     * @param viewHolder
+     * @param actionState
+     */
+    override fun onSelectedChanged(viewHolder: RecyclerView.ViewHolder?, actionState: Int) {
+        if (actionState != ItemTouchHelper.ACTION_STATE_IDLE) {
+            viewHolder?.itemView?.setBackgroundColor(Color.LTGRAY)
+        }
+        super.onSelectedChanged(viewHolder, actionState)
+    }
+
+    /**
+     * 手指松开的时候还原
+     * @param recyclerView
+     * @param viewHolder
+     */
+    override fun clearView(recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder) {
+        super.clearView(recyclerView!!, viewHolder)
+        viewHolder.itemView.setBackgroundColor(0)
+    }
+}
